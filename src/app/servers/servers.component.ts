1fa8fa4b428cf6f80651134182546d7ab1477814
Samsideen Adedoyin Quadri <squadri@venditan.com>
2019-07-27 11:03:08 +0100

    ng-if directive

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+serverName = 'Test server';
+serverCreation = 'No new server created';
+serverCreated = false;
 
+  onServerCreation() {
+    this.serverCreated = true;
+    return this.serverCreation = 'New server created with name ' + this.serverName;
+  }
   constructor() { }
 
   ngOnInit() {
